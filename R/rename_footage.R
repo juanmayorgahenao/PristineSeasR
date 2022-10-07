2f14a521eafd2494bd73171a455a07db584b1cc5
Juan Mayorga <js.mayorga368@gmail.com>
2022-10-07 14:32:53 -0500

    rename foo

--- R/rename_footage.R
+++ R/rename_footage.R
@@ -1,13 +1,13 @@
-rename_from_parent <- function(folder_path){
+rename_from_parent <- function(folder_path, from_ext, to_ext){
 
-  old_names <- list.files(folder_path, full.names = T)
+  old_names <- list.files(folder_path, full.names = T, pattern = from_ext)
 
   if(length(old_names) > 0){
     new_names <- file.path(folder_path,
                            paste0(basename(folder_path),
                                   "_c",
                                   formatC(seq(1:length(old_names)), width = 2, flag = 0),
-                                  ".MP4"))
+                                  to_ext))
 
     file.rename(old_names, new_names)
 
